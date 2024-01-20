883387d77b5f5481b4c877e0421666e23a5bb99f
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-01-19 21:58:30 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@
 
 export default function Home() {
   return (
-    <h1>Hello word</h1>
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl" >Hello </h1>
+      <h2>Hello </h2>
+      <p>Hello </p>
+
+
+    </div>
   );
 }
