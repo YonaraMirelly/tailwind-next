81eb8ed52c515b1363c270dcc65bd671ff19a86e
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-01-20 13:42:31 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl" >Hello </h1>
+      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+        Hello 
+      </h1>
       <h2>Hello </h2>
       <p>Hello </p>
 
