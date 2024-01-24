4909d1e59b7e62defaaa5fb20b7fd413eb9541d1
Yonara Mirelly <147000604+YonaraMirelly@users.noreply.github.com>
2024-01-24 13:48:43 -0300

    Upload do Avatar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,17 +11,21 @@ const config: Config = {
         profile: 'max-content 1fr min-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)'
       },
+      borderWidth: {
+        6: '6px', 
+      },
 
+      colors: {
+        violet: {
+          25: '#fcfaff', 
+        },
+        nara: '#8257e6',
+      },
       maxWidth: {
         app:'700px'
           
       },
 
-
-
-      colors: {
-        nara: '#8257e6',
-      }
     },
   },
   plugins: [],
